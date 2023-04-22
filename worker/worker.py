9ce463ceb0bd59654687dc1c8724ed4908917e14
ArmandoCivini <tcivini@gmail.com>
2023-04-22 19:17:59 -0300

    added rabbit

---
+++ worker/worker.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import pika
+import time
+import os
+
+# Wait for rabbitmq to come up
+time.sleep(10)
+
+consumer_id = os.environ["WORKER_ID"]
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='rabbitmq'))
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True)
+print('[{}] Waiting for messages. To exit press CTRL+C'.format(consumer_id))
+
+
+def callback(ch, method, properties, body):
+    print("[{}] Received {}".format(consumer_id, body))
+    if consumer_id == "1":
+        time.sleep(body.count(b'.'))
+    print("[{}] Done".format(consumer_id))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='task_queue', on_message_callback=callback)
+
+channel.start_consuming()
