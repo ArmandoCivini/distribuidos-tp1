72f9a6aa17d2a38e2afb7b2b0715fade610e5d4c
ArmandoCivini <tcivini@gmail.com>
2023-04-24 00:52:56 -0300

    improved worker

--- worker/main.py
+++ worker/main.py
@@ -3,12 +3,54 @@ import pika
 import time
 import os
 import logging
+from configparser import ConfigParser
+
+
+def initialize_config():
+    """ Parse env variables or config file to find program config params
+
+    Function that search and parse program configuration parameters in the
+    program environment variables first and the in a config file. 
+    If at least one of the config parameters is not found a KeyError exception 
+    is thrown. If a parameter could not be parsed, a ValueError is thrown. 
+    If parsing succeeded, the function returns a ConfigParser object 
+    with config parameters
+    """
+
+    config = ConfigParser(os.environ)
+    # If config.ini does not exists original config object is not modified
+    config.read("config.ini")
+
+    config_params = {}
+    try:
+        config_params["logging_level"] = os.getenv('LOGGING_LEVEL', config["DEFAULT"]["LOGGING_LEVEL"])
+    except KeyError as e:
+        raise KeyError("Key was not found. Error: {} .Aborting server".format(e))
+    except ValueError as e:
+        raise ValueError("Key could not be parsed. Error: {}. Aborting server".format(e))
+
+    return config_params
+
+def initialize_log(logging_level):
+    """
+    Python custom logging initialization
+
+    Current timestamp is added to be able to identify in docker
+    compose logs the date when the log has arrived
+    """
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging_level,
+        datefmt='%Y-%m-%d %H:%M:%S',
+    )
 
 def main():
-    # Wait for rabbitmq to come up
+    config_params = initialize_config()
+    logging_level = config_params["logging_level"]
+
+    initialize_log(logging_level)
+
     logging.info('started worker')
-    time.sleep(10)
-    logging.info('finished sleep')
     consumer_id = os.environ["WORKER_ID"]
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='rabbitmq'))
